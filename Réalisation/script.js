20530757053e3cdbf57c5efaf77c1338b99d5ca6
elyahyaoui-oubay <elyahyaoui.oubay@gmail.com>
2021-11-30 00:36:48 +0100

    added

--- Réalisation/script.js
+++ Réalisation/script.js
@@ -8,9 +8,9 @@ var tentative;
  // entrer: saisir 
 btn = document.getElementById('btn');
 sortir = document.getElementById('sortirtext');
- nombre = Math.floor(Math.random() * 100);
-console.log(number)
-tentativeSortir = document.getElementById('tentative')
+nombre = Math.floor(Math.random() * 100);
+console.log(nombre);
+tentativeSortir = document.getElementById('tentative');
 tentative = 10;
 
  // traitement 
@@ -19,22 +19,30 @@ tentative--;
 nombreDeviner = document.getElementById('entréeUtilisateur').value;
 if(tentative>0){
     tentativeSortir.innerHTML=tentative +"-tentative"
-  if (nombreDeviner == nombre && tentative >=3){
-    sortir.innerHTML = 'Supérieur'
-
-  } else{
-    if (nombreDeviner == nombre){
-        sortir.innerHTML = "Il est plus petit que"
+  if (nombreDeviner == nombre){
+    if ( nombreDeviner == nombre && tentative >=8){
+      sortir.innerHTML = 'Bravo, vous etes un génie !!'
+    }else{
+      if (nombreDeviner == nombre && tentative >=3){
+        sortir.innerHTML = 'Félicitations, vous aves gangné après , ' +tentative +"tentative"
+    }
     }
 
+ } else {
+   if (nombreDeviner < nombre){
+    sortir.innerHTML = "il est plus petit que"
+   }
     else {
-        sortir.innerHTML = "il est plus grand que"
+      sortir.innerHTML = "il est plus grand que"
+
+    }  
     }
+}else{
+  sortir.innerHTML = "C'est raté!!"
+  tentativeSortir.innerHTML = "0-tentative"
 }
 
-  } 
-
-});
+  });
 
 
 
