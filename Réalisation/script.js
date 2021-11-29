1e13430eb55223cee8e81c0034798211427f8b6f
elyahyaoui-oubay <elyahyaoui.oubay@gmail.com>
2021-11-29 16:51:22 +0100

    added

--- Réalisation/script.js
+++ Réalisation/script.js
@@ -6,23 +6,31 @@ var nombreDeviner;
 var tentative;
 
  // entrer: saisir 
- btn = document.getElementById('btn');
- sortir = document.getElementById('sortirtext');
+btn = document.getElementById('btn');
+sortir = document.getElementById('sortirtext');
  nombre = Math.floor(Math.random() * 100);
+console.log(number)
+tentativeSortir = document.getElementById('tentative')
+tentative = 10;
 
  // traitement 
 btn.addEventListener('click', function(){
-  tentative
-  nombreDeviner = document.getElementById('entréeUtilisateur').value;
-  if (nombreDeviner == nombre){
+tentative--;
+nombreDeviner = document.getElementById('entréeUtilisateur').value;
+if(tentative>0){
+    tentativeSortir.innerHTML=tentative +"-tentative"
+  if (nombreDeviner == nombre && tentative >=3){
     sortir.innerHTML = 'Supérieur'
+
   } else{
-    if (nombreDeviner < nombre){
+    if (nombreDeviner == nombre){
         sortir.innerHTML = "Il est plus petit que"
     }
+
     else {
-      sortir.innerHTML = "il est plus grand que"
+        sortir.innerHTML = "il est plus grand que"
     }
+}
 
   } 
 
